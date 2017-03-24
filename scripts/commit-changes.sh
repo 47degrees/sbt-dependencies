d6ee3bf61dfb3439f284df527b4634b72f535c5b
Fede Fernández <fede.f@47deg.com>
2017-03-24 18:07:23 +0100

    Modifies the file committing (#25)

--- scripts/commit-changes.sh
+++ scripts/commit-changes.sh
@@ -5,6 +5,8 @@ git config --global user.email "developer@47deg.com";
 git config --global user.name "47Deg (Travis CI)";
 git config --global push.default simple;
 
+git checkout master
+git pull --rebase
 git add AUTHORS.md CONTRIBUTING.md contributors.sbt LICENSE;
 git commit -m 'Updating policy files';
 git remote add travis-remote git@github.com:47deg/sbt-dependencies.git;
