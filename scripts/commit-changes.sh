397f40cc159cb3d4548a18417508f346f829e545
Fede Fernández <fede.f@47deg.com>
2017-03-24 17:18:42 +0100

    Fixes git remote command (#24)

--- scripts/commit-changes.sh
+++ scripts/commit-changes.sh
@@ -7,5 +7,5 @@ git config --global push.default simple;
 
 git add AUTHORS.md CONTRIBUTING.md contributors.sbt LICENSE;
 git commit -m 'Updating policy files';
-git remote set-url travis-remote git@github.com:47deg/sbt-dependencies.git;
+git remote add travis-remote git@github.com:47deg/sbt-dependencies.git;
 git push travis-remote master;
