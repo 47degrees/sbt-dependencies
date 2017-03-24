2c0435e54d481c40b5515d2e5ef840f7700ba770
Fede Fernández <fede.f@47deg.com>
2017-03-24 20:07:56 +0100

    Removes pull command (#26)

--- scripts/commit-changes.sh
+++ scripts/commit-changes.sh
@@ -6,7 +6,6 @@ git config --global user.name "47Deg (Travis CI)";
 git config --global push.default simple;
 
 git checkout master
-git pull --rebase
 git add AUTHORS.md CONTRIBUTING.md contributors.sbt LICENSE;
 git commit -m 'Updating policy files';
 git remote add travis-remote git@github.com:47deg/sbt-dependencies.git;
